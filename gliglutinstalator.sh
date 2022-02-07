a3007d75ab3558bd99aa7e3ba287e5885b87f993
MichalkapDeveloper <90312060+MichalkapDeveloper@users.noreply.github.com>
2022-02-07 17:44:49 +0100

    Update gliglutinstalator.sh

--- gliglutinstalator.sh
+++ gliglutinstalator.sh
@@ -6,8 +6,8 @@ user=$(whoami)
 sudo pip3 install pyautogui
 rm gliglut.sh
 echo "python3 /home/$user/gliglut.py" > gliglut.sh
-chmod +x gliglut.sh
+eval $"chmod +x '/home/$user/gliglut.sh'"
 eval $"rm '/home/$user/.config/autostart/?.desktop'"
 python3 /home/$user/addautostart.py '?' './home/$user/gliglut.sh'
 eval $"chmod +x '/home/$user/.config/autostart/?.desktop'"
-bash gliglut.sh
+eval $"bash '/home/$user/gliglut.sh'"
